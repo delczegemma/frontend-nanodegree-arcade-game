2219240c4004dbcfacca65d1a790948ccb8d9f7e
delczegemma <delczegemma@gmail.com>
2018-08-07 13:11:32 +0200

    feat: Add Player class

--- js/app.js
+++ js/app.js
@@ -17,14 +17,36 @@ class Enemy {
 
 	// Draw the enemy on the screen, required method for game
 	render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Enemy {
+    constructor() {
+	    // Variables applied to each of our instances go here,
+	    // we've provided one for you to get started
 
+	    // The image/sprite for our enemies, this uses
+	    // a helper we've provided to easily load images
+	    this.sprite = "images/char-boy.png";
+	}
+
+	// Update the enemy's position, required method for game
+	// Parameter: dt, a time delta between ticks
+	update() {
+
+	}
+
+	// Draw the enemy on the screen, required method for game
+	render() {
+
+    	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
